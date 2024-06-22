0054ca751223bb02631d05ead316447fa98fc2a9
Walyn <walyncurtis@gmail.com>
2024-06-22 11:50:34 -0500

    Adds projectile shooter

--- dejadungeon2/Assets/Scripts/Projectile.cs
+++ dejadungeon2/Assets/Scripts/Projectile.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed;
+    public float decayTime = 5f;
+    float spawnTime;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("collided with "+collision.name);
     }
 
+    public void Fired()
+    {
+        spawnTime = Time.time;
+    }
+
     void FixedUpdate()
     {
         transform.position += transform.right * speed * Time.deltaTime;
+        if (spawnTime + decayTime < Time.time ) gameObject.SetActive(false);
     }
 }
