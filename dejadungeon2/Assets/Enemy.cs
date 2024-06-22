534e8d5c07fe3fb8653a232b204a271806afd85d
Walyn <walyncurtis@gmail.com>
2024-06-22 13:20:22 -0500

    Adds patrolling enemy

---
+++ dejadungeon2/Assets/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform pathContainer;
+    Vector3 startPos;
+    int curWaypoint = 0;
+    public Rigidbody2D rb;
+    public float speed = 5f;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        float frameSpeed = speed * Time.deltaTime;
+        Vector3 targetPos = pathContainer.GetChild(curWaypoint).position;
+        rb.MovePosition(Vector3.MoveTowards(rb.position, targetPos, frameSpeed));
+        if (Vector3.Distance(rb.position, targetPos) <= 0.01f)
+        {
+            curWaypoint++;
+            curWaypoint %= pathContainer.childCount;
+        }
+    }
+
+    public void Reset()
+    {
+        transform.position = startPos;
+    }
+}
