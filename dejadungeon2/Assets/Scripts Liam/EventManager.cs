d48839f398327b0a376c7036be5cd06a61d183a3
Liam York <yorkliam08@gmail.com>
2024-06-22 15:58:17 -0500

    Created Level 1 and fixed the issue with the Event manager

--- dejadungeon2/Assets/Scripts Liam/EventManager.cs
+++ dejadungeon2/Assets/Scripts Liam/EventManager.cs
@@ -7,6 +7,11 @@ public class EventManager : MonoBehaviour
 {
     public static event Action ResetEvent;
 
+    void Awake()
+    {
+        ResetEvent = null;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
